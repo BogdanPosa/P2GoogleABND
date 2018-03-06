0da99228f011d2d664600b7ce124363418dd864c
Bogdan <posa.bogdan@yahoo.ro>
2018-03-06 22:00:28 +0200

    MainActivity.jave: new methods getScore, checkScore Values: new string file for romanian

--- app/src/main/java/com/example/android/courtcount/MainActivity.java
+++ app/src/main/java/com/example/android/courtcount/MainActivity.java
@@ -13,73 +13,94 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+    int finalValue;
+    boolean scorOK;
+    EditText textScore;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+   textScore = findViewById(R.id.TAthrow1);
     }
 
-    int scoreTeamA = 0;
-    int scoreTeamB = 0;
+
+    /**x
+     * This method takes the user input
+     */
+
+    public void getScore() {
+        String value = textScore.getText().toString();
+        if (!value.isEmpty()) {
+            finalValue = Integer.parseInt(value);
+        } else {
+            Context context = getApplicationContext();
+            Toast.makeText(context, "Please enter a numeric value!", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     /**
-     * Increase the score for Team A by 1 point.
+     * This method checks that the user input is ok
      */
 
 
-    public void addOneForTeamA(View v) {
-        EditText textScore = findViewById(R.id.TAthrow1);
-        String value = textScore.getText().toString();
-        int finalValue = Integer.parseInt(value);
+    public void checkScore() {
         if (finalValue < 101) {
-            scoreTeamA = scoreTeamA + finalValue;
-            displayForTeamA(scoreTeamA);
+            scorOK = true;
         } else {
+            scorOK = false;
             Context context = getApplicationContext();
             CharSequence text = "The score you entered is too big!";
             int duration = Toast.LENGTH_LONG;
             Toast toast = Toast.makeText(context, text, duration);
             toast.show();
         }
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+
+    public void addOneForTeamA(View v) {
+        getScore();
+        checkScore();
+        if (scorOK) {
+            scoreTeamA = scoreTeamA + finalValue;
+            displayForTeamA(scoreTeamA);
+        }
+        textScore.setText("");
     }
 
     /**
      * Increase the score for Team A by 2 points.
      */
     public void addTwoForTeamA(View v) {
-        EditText textScore = findViewById(R.id.TAthrow1);
-        String value = textScore.getText().toString();
-        int finalValue = Integer.parseInt(value);
-        if (finalValue < 101) {
+        getScore();
+        checkScore();
+        if (scorOK) {
             scoreTeamA = scoreTeamA + 2 * finalValue;
             displayForTeamA(scoreTeamA);
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "The score you entered is too big!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
         }
+        textScore.setText("");
+
     }
 
     /**
      * Increase the score for Team A by 3 points.
      */
     public void addThreeForTeamA(View v) {
-        EditText textScore = findViewById(R.id.TAthrow1);
-        String value = textScore.getText().toString();
-        int finalValue = Integer.parseInt(value);
-        if (finalValue < 101) {
+        getScore();
+        checkScore();
+        if (scorOK) {
             scoreTeamA = scoreTeamA + 3 * finalValue;
             displayForTeamA(scoreTeamA);
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "The score you entered is too big!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
         }
+        textScore.setText("");
+
     }
 
     /**
@@ -102,58 +123,39 @@ public class MainActivity extends AppCompatActivity {
      * Increase the score for Team B by 1 point.
      */
     public void addOneForTeamB(View v) {
-        EditText textScore = findViewById(R.id.TBthrow1);
-        String value = textScore.getText().toString();
-        int finalValue = Integer.parseInt(value);
-        if (finalValue < 101) {
+        getScore();
+        checkScore();
+        if (scorOK) {
             scoreTeamB = scoreTeamB + finalValue;
             displayForTeamB(scoreTeamB);
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "The score you entered is too big!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
         }
-
+        textScore.setText("");
     }
 
     /**
      * Increase the score for Team B by 2 points.
      */
     public void addTwoForTeamB(View v) {
-        EditText textScore = findViewById(R.id.TBthrow1);
-        String value = textScore.getText().toString();
-        int finalValue = Integer.parseInt(value);
-        if (finalValue < 101) {
+        getScore();
+        checkScore();
+        if (scorOK) {
             scoreTeamB = scoreTeamB + 2 * finalValue;
             displayForTeamB(scoreTeamB);
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "The score you entered is too big!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
         }
+        textScore.setText("");
     }
 
     /**
      * Increase the score for Team B by 3 points.
      */
     public void addThreeForTeamB(View v) {
-        EditText textScore = findViewById(R.id.TBthrow1);
-        String value = textScore.getText().toString();
-        int finalValue = Integer.parseInt(value);
-        if (finalValue < 101) {
+        getScore();
+        checkScore();
+        if (scorOK) {
             scoreTeamB = scoreTeamB + 3 * finalValue;
             displayForTeamB(scoreTeamB);
-        } else {
-            Context context = getApplicationContext();
-            CharSequence text = "The score you entered is too big!";
-            int duration = Toast.LENGTH_LONG;
-            Toast toast = Toast.makeText(context, text, duration);
-            toast.show();
         }
+        textScore.setText("");
     }
 
     /**
@@ -197,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method makes the keyboard disappear when clicking outside
+     * This method makes the keyboard disappear when losing focus
      */
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
